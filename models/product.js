0f07b61ce904a57657fedfadef363a13ea5f6476
manutdmohit <saudmohit@gmail.com>
2022-02-19 11:38:11 +0545

    Product Model

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'product name must be provided'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'product price must be provided'],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: 1,
+      max: 10,
+    },
+
+    company: {
+      type: String,
+      enum: {
+        values: ['ikea', 'liddy', 'caressa', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Product', productSchema);
