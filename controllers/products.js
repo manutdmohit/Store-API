02e61b11885bc79e6510dee516249d5ba7197e38
manutdmohit <saudmohit@gmail.com>
2022-02-20 15:21:39 +0545

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -48,6 +48,13 @@ exports.getAllProducts = async (req, res) => {
     result = result.select(selectList);
   }
 
+  // Page
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(200).json({ count: products.length, products });
