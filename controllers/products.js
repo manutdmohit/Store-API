279a2c56e954dd346f9d82ef2405d87a6e98a7d1
manutdmohit <saudmohit@gmail.com>
2022-02-20 14:19:07 +0545

    select option

--- controllers/products.js
+++ controllers/products.js
@@ -3,15 +3,17 @@ const Product = require('../models/product');
 exports.getAllProductsStatic = async (req, res) => {
   // const search = 'ai';
 
-  const products = await Product.find({
-    // name: { $regex: search, $options: 'i' },
-  }).sort('-price name');
+  // const products = await Product.find({
+  //   // name: { $regex: search, $options: 'i' },
+  // }).sort('-price name');
+
+  const products = await Product.find({}).select('name price');
 
   res.status(200).json({ count: products.length, products });
 };
 
 exports.getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -29,11 +31,18 @@ exports.getAllProducts = async (req, res) => {
   // console.log(queryObject);
 
   let result = Product.find(queryObject);
+
+  // sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const selectList = fields.split(',').join(' ');
+    result = result.select(selectList);
   }
 
   const products = await result;
